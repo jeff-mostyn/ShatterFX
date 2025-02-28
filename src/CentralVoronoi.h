90c32360d0f21b2a634f745f1d85e959e5758176
Jeff Mostyn <jeffmostyn2@gmail.com>
2025-02-27 19:26:18 -0500

    tore out a ton of code, SOP takes a 3d float input now and puts that in a local variable

--- src/CentralVoronoi.h
+++ src/CentralVoronoi.h
@@ -5,88 +5,95 @@
 
 //#include <GEO/GEO_Point.h>
 //
+
 #include <SOP/SOP_Node.h>
-#include "LSystem.h"
+
+#include "vec.h"
 
 namespace HDK_Sample {
-class SOP_CVD : public SOP_Node
-{
-public:
-    static OP_Node		*myConstructor(OP_Network*, const char *,
-							    OP_Operator *);
+    class SOP_CVD : public SOP_Node {
+    public:
+        static OP_Node		*myConstructor(OP_Network*, const char *,
+							        OP_Operator *);
 
-    /// Stores the description of the interface of the SOP in Houdini.
-    /// Each parm template refers to a parameter.
-    static PRM_Template		 myTemplateList[];
+        /// Stores the description of the interface of the SOP in Houdini.
+        /// Each parm template refers to a parameter.
+        static PRM_Template		 myTemplateList[];
 
-    /// This optional data stores the list of local variables.
-    static CH_LocalVariable	 myVariables[];
+        /// This optional data stores the list of local variables.
+        static CH_LocalVariable	 myVariables[];
 
-protected:
+    protected:
 
-    SOP_CVD(OP_Network *net, const char *name, OP_Operator *op);
-    virtual ~SOP_CVD();
+        SOP_CVD(OP_Network *net, const char *name, OP_Operator *op);
+        virtual ~SOP_CVD();
 
-    /// Disable parameters according to other parameters.
-    virtual unsigned		 disableParms();
+        /// Disable parameters according to other parameters.
+        virtual unsigned		 disableParms();
 
 
-    /// cookMySop does the actual work of the SOP computing, in this
-    /// case, a LSYSTEM
-    virtual OP_ERROR		 cookMySop(OP_Context &context);
+        /// cookMySop does the actual work of the SOP computing, in this
+        /// case, a LSYSTEM
+        virtual OP_ERROR		 cookMySop(OP_Context &context);
 
-    /// This function is used to lookup local variables that you have
-    /// defined specific to your SOP.
-    virtual bool		 evalVariableValue(
-				    fpreal &val,
-				    int index,
-				    int thread);
-    // Add virtual overload that delegates to the super class to avoid
-    // shadow warnings.
-    virtual bool		 evalVariableValue(
-				    UT_String &v,
-				    int i,
-				    int thread)
-				 {
-				     return evalVariableValue(v, i, thread);
-				 }
+        /// This function is used to lookup local variables that you have
+        /// defined specific to your SOP.
+        virtual bool		 evalVariableValue(
+				        fpreal &val,
+				        int index,
+				        int thread);
+        // Add virtual overload that delegates to the super class to avoid
+        // shadow warnings.
+        virtual bool		 evalVariableValue(
+				        UT_String &v,
+				        int i,
+				        int thread)
+				     {
+				         return evalVariableValue(v, i, thread);
+				     }
 
-private:
-    /// The following list of accessors simplify evaluating the parameters
-    /// of the SOP.
+    private:
+        /// The following list of accessors simplify evaluating the parameters
+        /// of the SOP.
 
-    // PUT YOUR CODE HERE
-	// Here you need to declare functions which need to be called from the .C file to 
-	// constantly update the cook function, these functions help you get the current value that the node has
-	// Example : To declare a function to fetch angle you need to do it this way 
-	// fpreal  ANGLE(fpreal t)     { return evalFloat("angle", 0, t); }
-    fpreal ANGLE(fpreal t) {
-        return evalFloat("angle", 0, t);
-    }
+        // PUT YOUR CODE HERE
+	    // Here you need to declare functions which need to be called from the .C file to 
+	    // constantly update the cook function, these functions help you get the current value that the node has
+	    // Example : To declare a function to fetch angle you need to do it this way 
+	    // fpreal  ANGLE(fpreal t)     { return evalFloat("angle", 0, t); }
+        /*fpreal ANGLE(fpreal t) {
+            return evalFloat("angle", 0, t);
+        }
 	
-    fpreal STEPSIZE(fpreal t) {
-        return evalFloat("stepSize", 0, t);
-    }
+        fpreal STEPSIZE(fpreal t) {
+            return evalFloat("stepSize", 0, t);
+        }
 
-    fpreal ITERATIONS(fpreal t) {
-        return evalFloat("iterations", 0, t);
-    }
+        fpreal ITERATIONS(fpreal t) {
+            return evalFloat("iterations", 0, t);
+        }
 
-    void GRAMMAR(UT_String& _str, fpreal t) {
-        return evalString(_str, "grammar", 0, t);
-    }
+        void GRAMMAR(UT_String& _str, fpreal t) {
+            return evalString(_str, "grammar", 0, t);
+        }*/
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        vec3 BOUNDS(fpreal t) {
+            float x = evalFloat("bounds", 0, t);
+            float y = evalFloat("bounds", 1, t);
+            float z = evalFloat("bounds", 2, t);
+            return vec3(x, y, z);
+        }
 
-    /// Member variables are stored in the actual SOP, not with the geometry
-    /// In this case these are just used to transfer data to the local 
-    /// variable callback.
-    /// Another use for local data is a cache to store expensive calculations.
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// NOTE : You can declare local variables here like this  
-    int		myCurrPoint;
-    int		myTotalPoints;
-};
+        /// Member variables are stored in the actual SOP, not with the geometry
+        /// In this case these are just used to transfer data to the local 
+        /// variable callback.
+        /// Another use for local data is a cache to store expensive calculations.
+
+	    // NOTE : You can declare local variables here like this  
+        std::vector < std::vector<vec3>> voronoiPoints;
+    };
 } // End HDK_Sample namespace
 
 #endif
