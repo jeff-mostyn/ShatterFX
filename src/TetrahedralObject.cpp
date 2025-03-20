e9e34d263a979b27612b5e494dad3515eaf412c1
Joanna Fisch <pojojojo21@gmail.com>
2025-03-19 21:24:53 -0400

    added min max

--- src/TetrahedralObject.cpp
+++ src/TetrahedralObject.cpp
@@ -1,12 +1,29 @@
 #include "TetrahedralObject.h"
 
-TetrahedralObject::TetrahedralObject() {
+TetrahedralObject::TetrahedralObject() : m_min(FLT_MAX, FLT_MAX, FLT_MAX), m_max(FLT_MIN, FLT_MIN, FLT_MIN)
+{
 	m_tets = std::vector<Tetrahedron*>();
+	//m_min = vec3(FLT_MAX, FLT_MAX, FLT_MAX);
+	//m_max = vec3(FLT_MIN, FLT_MIN, FLT_MIN);
 }
 
 TetrahedralObject::~TetrahedralObject() {
 	for (auto tet : m_tets) {
 		delete tet;
+	}
+}
+
+void checkTetMinMax(Tetrahedron* tet, vec3 &min, vec3 &max) {
+	min = vec3(FLT_MAX, FLT_MAX, FLT_MAX);
+	max = vec3(FLT_MIN, FLT_MIN, FLT_MIN);
+	for (vec3 point : tet->points)
+	{
+		min[0] = point[0] < min[0] ? point[0] : min[0];
+		min[1] = point[1] < min[1] ? point[1] : min[1];
+		min[2] = point[2] < min[2] ? point[2] : min[2];
+		max[0] = point[0] > max[0] ? point[0] : max[0];
+		max[1] = point[1] > max[1] ? point[1] : max[1];
+		max[2] = point[2] > max[2] ? point[2] : max[2];
 	}
 }
 
@@ -21,10 +38,21 @@ void TetrahedralObject::AddTet(std::vector<vec3> a_points) {
 			m_pointSet.insert(point);
 		}
 	}
+
+	vec3 min, max;
+	checkTetMinMax(tet, min, max);
+	m_min[0] = min[0] < m_min[0] ? min[0] : m_min[0];
+	m_min[1] = min[1] < m_min[1] ? min[1] : m_min[1];
+	m_min[2] = min[2] < m_min[2] ? min[2] : m_min[2];
+	m_max[0] = max[0] > m_max[0] ? max[0] : m_max[0];
+	m_max[1] = max[1] > m_max[1] ? max[1] : m_max[1];
+	m_max[2] = max[2] > m_max[2] ? max[2] : m_max[2];
 }
 
 void TetrahedralObject::DumpPoints() {
 	m_pointSet.clear();
+	m_min = vec3(FLT_MAX, FLT_MAX, FLT_MAX);
+	m_max = vec3(FLT_MIN, FLT_MIN, FLT_MIN);
 }
 
 std::set<vec3> TetrahedralObject::GetPointsSingleton() {
@@ -33,4 +61,14 @@ std::set<vec3> TetrahedralObject::GetPointsSingleton() {
 
 std::vector<Tetrahedron*> TetrahedralObject::GetTets() {
 	return m_tets;
+}
+
+vec3 TetrahedralObject::GetMin()
+{
+	return m_min;
+}
+
+vec3 TetrahedralObject::GetMax()
+{
+	return m_max;
 }
--- src/TetrahedralObject.h
+++ src/TetrahedralObject.h
@@ -16,9 +16,12 @@ public:
 	void DumpPoints();
 	std::set<vec3> GetPointsSingleton();
 	std::vector<Tetrahedron*> GetTets();
+	vec3 GetMin();
+	vec3 GetMax();
 
 private:
 	std::set<vec3> m_pointSet;	// we maintain a set so we make sure there are no duplicate points
 								// do we need this? would it be better to have duplicate points so things can be broken more easily?
 	std::vector<Tetrahedron*> m_tets;
+	vec3 m_min, m_max;			// maintain minimum and maximum ranges in the set
 };
