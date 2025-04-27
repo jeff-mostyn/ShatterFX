659573da9cbf2177f80adfd6b75b8a69fd8833e7
Jeff Mostyn <jeffmostyn2@gmail.com>
2025-04-27 14:03:25 -0400

    couple different ways of fusing
    One option is to spawn a fuse node, but apparently the `consolidatePoints` kinda does the same thing? it seems to work, anyway

--- src/ImpactPoint.cpp
+++ src/ImpactPoint.cpp
@@ -4,19 +4,21 @@
 //#include <UT/UT_DSOVersion.h>
 //#include <RE/RE_EGLServer.h>
 
-
-#include <UT/UT_Math.h>
-#include <UT/UT_Interrupt.h>
+#include <CH/CH_LocalVariable.h>
+#include <GA/GA_Primitive.h>
 #include <GU/GU_Detail.h>
 #include <GU/GU_PrimPoly.h>
-#include <CH/CH_LocalVariable.h>
-#include <PRM/PRM_Include.h>
-#include <PRM/PRM_SpareData.h>
+#include <GU/GU_PrimTetrahedron.h>
+#include <OBJ/OBJ_Node.h>
+#include <OP/OP_Director.h>
+#include <OP/OP_Network.h>
+#include <OP/OP_Node.h>
 #include <OP/OP_Operator.h>
 #include <OP/OP_OperatorTable.h>
-#include <GU/GU_PrimTetrahedron.h>
-#include <GA/GA_Primitive.h>
-#include <OP/OP_Director.h>
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_SpareData.h>
+#include <UT/UT_Interrupt.h>
+#include <UT/UT_Math.h>
 
 #include <limits.h>
 #include "ImpactPoint.h"
@@ -31,6 +33,8 @@ static PRM_Name forceMagName("forceMag", "Impact Force Magnitude (N)");
 static PRM_Name forceDirName("forceDir", "Impact Force Direction");
 static PRM_Name forceLocName("forceLoc", "Impact Force Location");
 static PRM_Name pickButtonName("getpos", "Get Selected Point Position");
+static PRM_Name filenameName("outputFile", "Output File (no suffix)");
+static PRM_Name exportButtonName("export", "Export File");
 
 // SET PARAMETER DEFAULTS
 
@@ -85,25 +89,6 @@ SOP_Impact::myVariables[] = {
 bool
 SOP_Impact::evalVariableValue(fpreal& val, int index, int thread)
 {
-	// myCurrPoint will be negative when we're not cooking so only try to
-	// handle the local variables when we have a valid myCurrPoint index.
-	/*
-	if (myCurrPoint >= 0) {
-	// Note that "gdp" may be null here, so we do the safe thing
-	// and cache values we are interested in.
-		switch (index)
-		{
-			case VAR_PT:
-				val = (fpreal) myCurrPoint;
-				return true;
-			case VAR_NPT:
-				val = (fpreal) myTotalPoints;
-				return true;
-			default:
-			// do nothing ;
-		}
-	}
-	*/
 	// Not one of our variables, must delegate to the base class.
 	return SOP_Node::evalVariableValue(val, index, thread);
 }
@@ -179,8 +164,6 @@ SOP_Impact::cookMySop(OP_Context& context)
 	vec3 forceLoc;
 	forceLoc = FORCE_LOC(now);
 
-
-	//std::cout << "---------------------------------------------" << std::endl;
 
 	// PROCESS DATA HERE
 	// Normalize the direction vector
@@ -286,34 +269,62 @@ int SOP_Impact::PickCallback(void* data, int index,
 	// use this link for help setting up next node https://www.sidefx.com/docs/hdk/_h_d_k__node_intro__working_with_nodes.html
 	
 	// Variables
-	OP_Network* parent;
-	OP_Node* node;
+	OP_Network* parent = (OP_Network*)(sop->getParent());
+	OP_Node* geo = OPgetDirector()->findNode("/obj/geo1");
+	OP_Node* shatterNode;
+	OP_Node* convertTetsNode;
+	OP_Node* fuseNode;
 	OP_Node* input;
 
+	// ---------------------------------------------------------
+	//					Create & Attach CVD Node
+	// ---------------------------------------------------------
 	// create node
-	parent = (OP_Network*)OPgetDirector()->findNode("/obj/geo1");
-	node = parent->createNode("CusCentroidalVoronoi", "CVD1");
+	shatterNode = parent->createNode("CusCentroidalVoronoi", "CVD1");
 	
 	// set parameters
-	node->setFloat("forceMag", 0, time, forceMag);
+	shatterNode->setFloat("forceMag", 0, time, forceMag);
 
-	node->setFloat("forceDir", 0, time, forceDir[0]);
-	node->setFloat("forceDir", 1, time, forceDir[1]);
-	node->setFloat("forceDir", 2, time, forceDir[2]);
+	shatterNode->setFloat("forceDir", 0, time, forceDir[0]);
+	shatterNode->setFloat("forceDir", 1, time, forceDir[1]);
+	shatterNode->setFloat("forceDir", 2, time, forceDir[2]);
 
-	node->setFloat("forceLoc", 0, time, forceLoc[0]);
-	node->setFloat("forceLoc", 1, time, forceLoc[1]);
-	node->setFloat("forceLoc", 2, time, forceLoc[2]);
+	shatterNode->setFloat("forceLoc", 0, time, forceLoc[0]);
+	shatterNode->setFloat("forceLoc", 1, time, forceLoc[1]);
+	shatterNode->setFloat("forceLoc", 2, time, forceLoc[2]);
 
 	// connect the node
 	input = parent->findNode("tetconform1");  // find /obj/geo1/tetConform1 as relative path
 	if (input)
 	{
-		node->setInput(0, input);       // set first input to /obj/tetConform
+		shatterNode->setInput(0, input);       // set first input to /obj/tetConform
 	}
 
+	// --------------------------------------------- ------------
+	//					Create & Attach Fuse Node
+	// ---------------------------------------------------------
+	// create node
+	/*fuseNode = parent->createNode("fuse", "Fuse1");
+
+	if (!fuseNode) {
+		std::cout << "Failed to fuse node!" << std::endl;
+	}
+	else {
+		std::cout << "Successfully created fuse node." << std::endl;
+
+		// set parameters
+		fuseNode->getParm("tol3d").setValue(1.0f, 0, time);
+
+		// connect the node
+		fuseNode->setInput(0, shatterNode);
+	}*/
+
+	// ---------------------------------------------------------
+	//						Arrange Nodes
+	// ---------------------------------------------------------
 	// now that done we're done connecting it, position it relative to its inputs
-	node->moveToGoodPosition();
+	shatterNode->moveToGoodPosition();
+	//fuseNode->moveToGoodPosition();
 
 	sop->forceRecook(); // Trigger cookMySop again
 	return 1;
